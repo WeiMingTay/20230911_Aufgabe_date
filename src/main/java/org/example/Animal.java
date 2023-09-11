5ce73b9d97707a354d91aa51b9738e292a572dbc
WeiMing <110397919+WeiMingTay@users.noreply.github.com>
2023-09-11 16:00:46 +0200

    Bonus Aufgaben: Klasse angelegt und Instanz hinzugefügt

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Animal {
+    private String name;
+    private LocalDate birthday;
+
+    public Animal(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,57 +11,60 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== aktuelle Datum ===");
         LocalDateTime now = LocalDateTime.now();
-        System.out.println(now);
+        // System.out.println(now);
         System.out.println(
                 now.getDayOfMonth()
-                +". "
-                +now.getMonth()
-                +" "
-                +now.getYear());
+                        + ". "
+                        + now.getMonth()
+                        + " "
+                        + now.getYear());
         System.out.println(
                 now.getHour()
-                +":"
-                +now.getMinute()
-                +":"
-                +now.getSecond()
+                        + ":"
+                        + now.getMinute()
+                        + ":"
+                        + now.getSecond()
         );
 
         int weeks = 2;
-        System.out.println("=== plus "+weeks+" Wochen ===");
+        System.out.println("=== plus " + weeks + " Wochen ===");
         LocalDateTime newNow = now.plusWeeks(weeks);
-        System.out.println(newNow);
+        // System.out.println(newNow);
         System.out.println(
                 newNow.getDayOfMonth()
-                        +". "
-                        +newNow.getMonth()
-                        +" "
-                        +newNow.getYear());
+                        + ". "
+                        + newNow.getMonth()
+                        + " "
+                        + newNow.getYear());
 
         System.out.println("=== vor oder nach? ===");
         System.out.println("--- deutsches Format ---");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd. MMMM yyyy", Locale.GERMAN);
         LocalDate twoYears = LocalDate.of(2023, 10, 25);
         String formattedDate = twoYears.format(formatter);
-        System.out.println("<3 "+formattedDate+" <3");
+        System.out.println("<3 " + formattedDate + " <3");
 
         LocalDate dateNow = LocalDate.now();
-        System.out.println(dateNow);
-        System.out.println(twoYears);
+        // System.out.println(dateNow);
+        // System.out.println(twoYears);
 
         int compareDate = dateNow.compareTo(twoYears);
-long daysDifference = ChronoUnit.DAYS.between(dateNow, twoYears);
-if(daysDifference < 0) {
-    daysDifference = daysDifference * -1;
-}
+        long daysDifference = ChronoUnit.DAYS.between(dateNow, twoYears);
+        if (daysDifference < 0) {
+            daysDifference = daysDifference * -1;
+        }
 
-        if(compareDate <0 ) {
-            System.out.println(dateNow.format(formatter)+" liegt "+ daysDifference+ " Tage \u001B[32mvor\u001B[0m dem "+twoYears.format(formatter));
+        if (compareDate < 0) {
+            System.out.println(dateNow.format(formatter) + " liegt " + daysDifference + " Tage \u001B[32mvor\u001B[0m dem " + twoYears.format(formatter));
         } else if (compareDate > 0) {
-            System.out.println(dateNow.format(formatter)+ " liegt "+ daysDifference+ " Tage \u001B[32mnach\u001B[0m dem "+twoYears.format(formatter));
+            System.out.println(dateNow.format(formatter) + " liegt " + daysDifference + " Tage \u001B[32mnach\u001B[0m dem " + twoYears.format(formatter));
         } else {
             System.out.println("Beide Tage sind gleich");
         }
 
+        System.out.println("=== === Bonus Aufgaben === ===");
+        Animal dimSum = new Animal("Dim Sum", LocalDate.of(2016, 4, 1));
 
+        System.out.println(dimSum);
     }
 }

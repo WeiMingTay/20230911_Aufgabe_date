ce8073e587829aafaa4e621b91a5c593bd93ff40
WeiMing <110397919+WeiMingTay@users.noreply.github.com>
2023-09-11 15:10:53 +0200

    Schritt 1: Aktuelle Datum und Uhrzeit ausgegeben

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        System.out.println(
+                now.getDayOfMonth()
+                +". "
+                +now.getMonth()
+                +"."
+                +now.getYear());
+        System.out.println(
+                now.getHour()
+                +":"
+                +now.getMinute()
+                +":"
+                +now.getSecond()
+        );
     }
 }
